8f04a3102d23622a24521c9b611bb65d1e42394c
Andrey Makov <one1l@yandex.ru>
2022-06-21 14:57:49 +0300

    Updates

--- src/components/Button/index.js
+++
@@ -1,8 +0,0 @@
-import './styles.scss';
-
-const Button = ({theme, children, onClick, className}) =>
-  <div onClick={onClick} className={`button button_theme_${theme} ${className}`}>
-    {children}
-  </div>;
-
-export default Button;
--- src/components/StyledInput/index.js
+++
@@ -1,9 +0,0 @@
-import { Input } from '@mui/material';
-import './styles.scss';
-
-const StyledInput = (props) =>
-  <Input {...props}
-    className={`styled-input ${props.className}`} classes={{input: 'styled-input__input'}}
-  />;
-
-export default StyledInput;
